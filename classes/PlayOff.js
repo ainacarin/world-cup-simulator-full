e61b78c0b8bdf765580c740a7e7648d9ed8fdeeb
Aida N <nacarinobazaga@gmail.com>
2020-12-27 12:28:09 +0100

    Añado versión inicial de programa con PlayOff sin mostrar ganadores

---
+++ classes/PlayOff.js
@@ -0,0 +1,129 @@
+Array.prototype.shuffle = function () {
+  var i = this.length;
+  while (i) {
+    var j = Math.floor(Math.random() * i);
+    var t = this[--i];
+    this[i] = this[j];
+    this[j] = t;
+  }
+  return this;
+};
+
+const NUMBER_TOTAL_TEAMS_PLAYOFF = 16;
+const NUMBER_TOTAL_MATCHES_PLAYOFF = 5;
+
+export default class PlayOff {
+  constructor(name, teams = [], config = {}) {
+    this.name = name;
+    this.teams = [];
+    this.config = {};
+    this.setup(teams, config);
+    this.totalDataPlayOff = [];
+    this.winnersPlayOff = [];
+  }
+
+  setupConfig(config) {
+    const defaultConfig = { rounds: 1 };
+    this.config = Object.assign(defaultConfig, config);
+  }
+
+  setupTeams(teams) {
+    teams.shuffle();
+    for (let index = 0; index < NUMBER_TOTAL_TEAMS_PLAYOFF; index++) {
+      const team = teams[index];
+      this.teams.push(team);
+    }
+  }
+
+  setup(teams, config) {
+    this.setupTeams(teams);
+    this.setupConfig(config);
+  }
+
+  generateResultGoals() {
+    return Math.round(Math.random() * 10);
+  }
+
+  playMatch(match) {
+    do {
+      match.resultLocalTeam = this.generateResultGoals();
+      match.resultVisitTeam = this.generateResultGoals();
+    } while (match.resultLocalTeam == match.resultVisitTeam);
+
+    return match;
+  }
+
+  playOnePlayOff(playOff) {
+    for (const match of playOff) {
+      this.playMatch(match);
+    }
+  }
+
+  getLosersOnePlayOff(playOff) {
+    const loserTeam = [];
+    for (const match of playOff) {
+      if (match.resultLocalTeam < match.resultVisitTeam) {
+        loserTeam.push(match.localTeam);
+      } else {
+        loserTeam.push(match.visitTeam);
+      }
+    }
+    return loserTeam;
+  }
+
+  getWinnersOnePlayOff(playOff) {
+    const winnerTeams = [];
+    for (const match of playOff) {
+      if (match.resultLocalTeam > match.resultVisitTeam) {
+        winnerTeams.push(match.localTeam);
+      } else {
+        winnerTeams.push(match.visitTeam);
+      }
+    }
+    return winnerTeams;
+  }
+
+  createPlayOff(teams) {
+    const playOff = [];
+
+    //por cada equipo se añade a local o a visit
+    let indexMatch = 0;
+    for (let i = 0; i < teams.length; i++) {
+      if (i % 2 == 0) {
+        const match = {
+          localTeam: teams[i],
+          visitTeam: "",
+          resultLocalTeam: 0,
+          resultVisitTeam: 0,
+        };
+        playOff.push(match);
+      } else {
+        playOff[indexMatch].visitTeam = teams[i];
+        indexMatch++;
+      }
+    }
+    console.log(playOff);
+    return playOff;
+  }
+
+  start() {
+    let teamsOnePlayOff = this.teams.map((team) => team);
+    //3 phases playoff
+    for (let index = 0; index < NUMBER_TOTAL_MATCHES_PLAYOFF; index++) {
+      const playOff = this.createPlayOff(teamsOnePlayOff);
+      this.playOnePlayOff(playOff);
+      if (index == NUMBER_TOTAL_MATCHES_PLAYOFF - 3) {
+        //semifinal phase
+        teamsOnePlayOff = this.getLosersOnePlayOff(playOff);
+      } else if (index == NUMBER_TOTAL_MATCHES_PLAYOFF - 2) {
+        //final phase
+        teamsOnePlayOff = this.getWinnersOnePlayOff(this.totalDataPlayOff[index - 1]);
+      } else {
+        //default phase
+        teamsOnePlayOff = this.getWinnersOnePlayOff(playOff);
+      }
+      this.winnersPlayOff.push(teamsOnePlayOff);
+      this.totalDataPlayOff.push(playOff);
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,62 @@
+import WorldCupSimulator from "./classes/PlayOff.js";
+
 console.log("World Cup Simulator");
+
+const nameWorldCup = "World Cup Spain";
+const teams = [
+  "1A",
+  "1B",
+  "1C",
+  "1D",
+  "1E",
+  "1F",
+  "1G",
+  "1H",
+  "1I",
+  "1J",
+  "1K",
+  "1L",
+  "1M",
+  "1N",
+  "1O",
+  "1P",
+  "1Q",
+  "1R",
+  "1S",
+  "1T",
+  "1U",
+  "1V",
+  "1W",
+  "1X",
+  "1Y",
+  "1Z",
+  "2A",
+  "2B",
+  "2C",
+  "2D",
+  "2E",
+  "2F",
+];
+const config = { rounds: 1 };
+const titlesPlayOff = [
+  "OCTAVOS DE FINAL",
+  "CUARTOS DE FINAL",
+  "SEMIFINALES",
+  "TERCER Y CUARTO PUESTO",
+  "FINAL",
+];
+
+const playOff = new WorldCupSimulator(nameWorldCup, teams, config);
+playOff.start();
+
+console.log("NAME COMPETITION", playOff.name);
+console.log("TEAMS", playOff.teams);
+/* console.log('TOTAL DATA PLAYSOFF',playOff.totalDataPlayOff); */
+console.log("TOTAL DATA PLAYSOFF");
+for (let i = 0; i < playOff.totalDataPlayOff.length; i++) {
+  console.log(`==== ${titlesPlayOff[i]} ${i} ====`);
+  for (const match of playOff.totalDataPlayOff[i]) {
+        console.log(`${match.localTeam} ${match.resultLocalTeam} - ${match.visitTeam} ${match.resultVisitTeam} => `);
+    }
+}
+console.log('WINNERS', playOff.winnersPlayOff);
